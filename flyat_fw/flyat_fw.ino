d11c01299430253e4b1d139d0bcd54c60f1d7c98
Elias Rhoden <eliasr@student.chalmers.se>
2022-01-02 13:30:06 +0100

    More comments + simple example 2 tasks

--- flyat_fw/flyat_fw.ino
+++ flyat_fw/flyat_fw.ino
@@ -1,76 +1,54 @@
 
-// Include Arduino FreeRTOS library
 #include <Arduino_FreeRTOS.h>
-
-// Include mutex support
 #include <semphr.h>
 
-/*
-   Declaring a global variable of type SemaphoreHandle_t
-*/
+#define radio_Ts 1000
+#define servo_Ts 500
 
-SemaphoreHandle_t mutex;
 
-int globalCount = 0;
+SemaphoreHandle_t setpoints_mutex;
+
+volatile float setpoint_pitch;
 
 void setup() {
 
   Serial.begin(9600);
+  Serial.println("LETSGO");
 
-  /**
-       Create a mutex.
-       https://www.freertos.org/CreateMutex.html
-  */
-  mutex = xSemaphoreCreateMutex();
-  if (mutex != NULL) {
+  setpoints_mutex = xSemaphoreCreateMutex();
+  if (setpoints_mutex != NULL) {
     Serial.println("Mutex created");
   }
 
   /**
      Create tasks
   */
-  xTaskCreate(TaskMutex, // Task function
-              "Task1", // Task name for humans
-              128, 
+  xTaskCreate(setpoint_from_radio, // Task function
+              "RadioTask", // Task name for humans
+              128,  // stack depth
               1000, // Task parameter
-              1, // Task priority
+              1, // Task priority (HIGH = VIP, Low = pleb)
               NULL);
 
-  xTaskCreate(TaskMutex, "Task2", 128, 1000, 1, NULL);
+  xTaskCreate(servo_ctrl, "ServoTask", 128, 1000, 2, NULL);
 
 }
 
 void loop() {}
 
-void TaskMutex(void *pvParameters)
-{
-  int delay = *((int*)pvParameters); // Use task parameters to define delay
+void setpoint_from_radio(void *pvParameters){
+  //xSemaphoreGive(mutex);
+      while(true){
+            Serial.println("radio boi");
+            vTaskDelay(radio_Ts / portTICK_PERIOD_MS);
+      }
+      
+}
 
-  for (;;)
-  {
-    /**
-       Take mutex
-       https://www.freertos.org/a00122.html
-    */
-    if (xSemaphoreTake(mutex, 10) == pdTRUE)
-    {
-      Serial.print(pcTaskGetName(NULL)); // Get task name
-      Serial.print(", Count read value: ");
-      Serial.print(globalCount);
+void servo_ctrl(void *pvParameters){
+      while(true){
+            Serial.println("Servoboi");
+            vTaskDelay(servo_Ts / portTICK_PERIOD_MS);
+      }
 
-      globalCount++;
-
-      Serial.print(", Updated value: ");
-      Serial.print(globalCount);
-
-      Serial.println();
-      /**
-         Give mutex
-         https://www.freertos.org/a00123.html
-      */
-      xSemaphoreGive(mutex);
-    }
-
-    vTaskDelay(delay / portTICK_PERIOD_MS);
-  }
 }
