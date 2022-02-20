77d7c76199c11c6656329adfc4e15ff217182aab
Elias Rhoden <eliasr@student.chalmers.se>
2022-02-20 15:03:43 +0100

    smol coment

--- Develop/sd_card_soft_spi/sd_card_soft_spi.ino
+++ Develop/sd_card_soft_spi/sd_card_soft_spi.ino
@@ -7,6 +7,8 @@
 //#define SPI_DRIVER_SELECT 2
 #include "SdFat.h"
 
+// dis is smol coment
+
 #if SPI_DRIVER_SELECT == 2  // Must be set in SdFat/SdFatConfig.h
 
 // SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
